28db14be8195df4715da18aede842aed4619a288
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-19 20:58:31 +0500

    used the data from app.js to social card

--- socialcard/src/components/socialcard.js
+++ socialcard/src/components/socialcard.js
@@ -4,8 +4,18 @@ class SocialCard extends React.Component {
     render() {
         return (
             <div>
+                {/* Title Bar */}
                 Users: {this.props.user}
+                <br />
                 Email:{this.props.email}
+                <br />
+
+                {/* TextContainer */}
+                message:{this.props.message}
+                <br />
+                {/* Picture */}
+                image:{this.props.img}
+                <br />
             </div>
         )
     }
