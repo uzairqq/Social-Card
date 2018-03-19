1f168208560c757d8d0b26621b798b41eb56d2ce
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-19 16:42:28 +0500

    fetched the data from a json file and used that data in a table

--- socialcard/src/App.js
+++ socialcard/src/App.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      alldata: [],
+      error: null
+    }
+  }
+  componentDidMount() {
+    fetch('/data/userMessages.json')
+      .then(response => response.json())
+      .then(data => this.setState({ alldata: data }))
+      .catch(error => this.setState({ error }));
+  }
   render() {
+    var userName = this.state.alldata.map((content) => {
+      return <li key={content.user}>{content.user}</li>
+    })
+    var email = this.state.alldata.map((content) => {
+      return <li key={content.user}>{content.email}</li>
+    })
     return (
-      <div></div>
+      <Fragment>
+        <div id="theme">
+          <img src="/images/title.png" alt="Social App Icon" className="img-title" />
+        </div>
+        {this.state.error && <p>{this.state.error.message}</p>}
+        <table>
+          <tbody>
+            <tr>
+              <th>UserName</th>
+              <th>Email</th>
+            </tr>
+            <tr>
+              <td>{userName}</td>
+              <td>{email}</td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
+      </Fragment>
     );
   }
 }
